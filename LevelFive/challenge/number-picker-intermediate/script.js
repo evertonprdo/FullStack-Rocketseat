50c64729c362a2fc8e5f1b7da3a52ed0935ca737
evertonprdo <ecpj.prado@gmail.com>
2024-11-25 09:08:51 -0300

    Execution Flow

---
+++ LevelFive/challenge/number-picker-intermediate/script.js
@@ -0,0 +1,24 @@
+/**
+ *  Execution Flow
+ * 
+ *  User chooses the amount number, range and if is repeatable.
+ *    QuerySelector inputs
+ * 
+ *  On draw click, the numbers are drawn and the draw counter is incremented.
+ *    class NumberPicker.draw()
+ *  
+ *  The screen switches to the results screen and the entry animation plays.
+ *     hidden input screen, shown result screen  
+ * 
+ *  After all the numbers are displayed the draw again button appears
+ *    shown draw again
+ *  
+ *  On click draw again, return to the input screen.
+ *    hidden result screen and draw again btn, shown input screen
+ * 
+ *  ======
+ * 
+ *  A NumberPicker class responsible for storing business rules
+ *  Screen switching animations with event-based results
+ * 
+ */
